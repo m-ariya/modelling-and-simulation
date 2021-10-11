61e16b17062b8d52e66e1d5c9559639f1c4ba396
Jeroen <jeroenlammers1@gmail.com>
2021-10-11 12:39:50 +0200

    Added accuracy plot and fixed some bugs

--- testAccuracy.m
+++ testAccuracy.m
@@ -5,24 +5,47 @@ img_cir = rgb2gray(imread('Images/circle.png')) / 255;
 img_squ = rgb2gray(imread('Images/square.png')) / 255;
 img_tri = rgb2gray(imread('Images/triangle.png')) / 255;
 img_cre = rgb2gray(imread('Images/creeper.png')) / 255;
+img_dia = rgb2gray(imread('Images/diamond.png')) / 255;
+img_sta = rgb2gray(imread('Images/star.png')) / 255;
+img_crc = rgb2gray(imread('Images/crecent.png')) / 255;
+img_pic = rgb2gray(imread('Images/pickaxe.png')) / 255;
+img_che = rgb2gray(imread('Images/checker.png')) / 255;
 
 % Rescale the matrices to vectors ant convert from uint8 to double
 cir = double(reshape(img_cir,1,[]));
 squ = double(reshape(img_squ,1,[]));
 tri = double(reshape(img_tri,1,[]));
 cre = double(reshape(img_cre,1,[]));
+dia = double(reshape(img_dia,1,[]));
+sta = double(reshape(img_sta,1,[]));
+crc = double(reshape(img_crc,1,[]));
+pic = double(reshape(img_pic,1,[]));
+che = double(reshape(img_che,1,[]));
 
 % Transform to [-1 1] 
 cir(cir == 0) = -1;
 squ(squ == 0) = -1;
 tri(tri == 0) = -1;
 cre(cre == 0) = -1;
+dia(dia == 0) = -1;
+sta(sta == 0) = -1;
+crc(crc == 0) = -1;
+pic(pic == 0) = -1;
+che(che == 0) = -1;
 
 % Define memory array
 % n: #neurons, m: #memories
-memories = [cre;cir;squ;tri];
-memories_names = ["creeper", "circle", "square", "triangle"];
+memories = [cre;cir;squ;tri];%;dia;sta;crc;pic;che];
+memories_names = ["creeper", "circle", "square", "triangle"];%, "diamond", "star", "crecent", "pickaxe", "checkers"];
 [m,n] = size(memories);
+
+% Compute the distance between memories
+dist_memories = zeros(m,m);
+for i = 1:m
+    for j = 1:m
+        dist_memories(i,j) = hamdist(memories(i,:),memories(j,:));
+    end
+end
 
 %% ------ Train models ------
 hebi_net = HopfieldNet(64, 'Hebbian');
@@ -44,7 +67,7 @@ stor_output = zeros(n+1,n+1,m);
 proj_output = zeros(n+1,n+1,m);
 
 %% ------ Run simulation ------
-num_iter = 10000;
+num_iter = 1000;
 dist_start = 0;
 dist_end = 64;
 
@@ -81,13 +104,34 @@ for mem_idx = 1:m
     fprintf('Finished memory -%s-\n', memories_names(mem_idx))
 end
 
-%% ------ Normalise data ------
+%% ------ Prepare data ------
+% Combine the data of each memory for a accuracy plot
+hebi_error = hebi_output(:,1,1);
+stor_error = stor_output(:,1,1);
+proj_error = proj_output(:,1,1);
+
+% Add all samples to a single matrix
+hebi_total_out = zeros(n+1,n+1);
+stor_total_out = zeros(n+1,n+1);
+proj_total_out = zeros(n+1,n+1);
+for mem_idx = 1:m
+    hebi_total_out = hebi_output(:,:,mem_idx);
+    stor_total_out = stor_output(:,:,mem_idx);
+    proj_total_out = proj_output(:,:,mem_idx);
+end
+% Normalise the total matrices
+for idx = 1:n+1
+    hebi_total_out(idx,:) = hebi_total_out(idx,:) / sum(hebi_total_out(idx,:));
+    stor_total_out(idx,:) = stor_total_out(idx,:) / sum(stor_total_out(idx,:));
+    proj_total_out(idx,:) = proj_total_out(idx,:) / sum(proj_total_out(idx,:));
+end
+
 % Normalise each  row of the output plot such that we see the persentages
 % instead of a heatmap
 for mem_idx = 1:m
     for n_idx = 1:n+1
         % Hebbian
-        maxVal = max(hebi_output(n_idx,:,mem_idx));
+        maxVal = sum(hebi_output(n_idx,:,mem_idx));
         hebi_output(n_idx,:,mem_idx) = hebi_output(n_idx,:,mem_idx) / maxVal;
         
         % Storkey
@@ -133,3 +177,16 @@ for i = 1:m
     ylabel('Hamming distance of input to memory')
     colorbar
 end
+
+%% ------ Plot accuracy ------
+figure('Name','Accuracy')
+plot(1:n+1,hebi_total_out(:,1),'-o')
+hold on
+plot(1:n+1,stor_total_out(:,1),'-o')
+plot(1:n+1,proj_total_out(:,1),'-o')
+hold off
+xlabel('Number of bits flipped in input')
+ylabel('Accuracy')
+legend('Hebbian', 'Storkey', 'Pseudo-inverse')
+grid on
+grid minor
