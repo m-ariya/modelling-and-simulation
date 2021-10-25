fbd9a89fac7493c22043ee9e6b2df55172f88029
m-ariya <49652030+m-ariya@users.noreply.github.com>
2021-10-25 20:07:51 +0200

    upd of visual.m

--- Matlab/examples/visual.m
+++ Matlab/examples/visual.m
@@ -17,11 +17,94 @@ cre(cre == 0) = -1;
 
 memories = [cre;cir;squ;tri];
 
-net = HopfieldNet(64, 'Hebbian');
-net = net.train(memories);
+tst(memories)
 
-m = distortmem(memories(1,:), 20);
-mr = reshape(m, 8,8);
-imagesc(mr); axis square; colormap bone; 
-net.reconstructVisually(m);
 
+function tst(memories)
+rule = 'Hebbian';
+pattern = 'Creeper';
+m = 1;
+distortionLevel = 0;
+distorted = zeros(1,64);
+
+f = figure;
+img = reshape(memories(m,:), 8, 8);
+imagesc(img); 
+axis square;
+colormap bone;
+drawnow;
+title('Innitially flipped bits: 0')
+
+cRule = uicontrol(f,'Style','popupmenu');
+cRule.Position = [20 370 60 20];
+cRule.String = {'Hebbian','Storkey','Projection'};
+cRule.Callback = @selectRule;
+
+cPattern = uicontrol(f,'Style','popupmenu');
+cPattern.Position = [20 350 60 20];
+cPattern.String = {'Creeper','Circle','Square', 'Triangle'};
+cPattern.Callback = @selectPattern;
+
+cDist = uicontrol(f,'Style','slider', 'Max',64,'Min',0, 'Value', distortionLevel);
+cDist.Position = [20 330 60 20];
+cDist.Callback = @store;
+
+cButton = uicontrol;
+cButton.Position = [20 310 60 20];
+cButton.String = 'Reconstruct';
+cButton.Callback = @reconstruct;
+
+    function  selectRule(src,event)
+        val = cRule.Value;
+        str = cRule.String;
+        str{val};
+        disp(['Selection: ' str{val}]);
+        rule = str{val};
+    end
+
+    function  selectPattern(src,event)
+        val = cPattern.Value;
+        str = cPattern.String;
+        str{val};
+        disp(['Selection: ' str{val}]);
+        pattern =  str{val};
+        
+        switch pattern
+            case 'Creeper'
+                m = 1;
+            case 'Circle'
+                m = 2;
+            case 'Square'
+                m = 3;
+            case 'Triangle'
+                m = 4;
+        end
+        
+        img = reshape(memories(m,:), 8, 8);
+        imagesc(img); 
+        axis square;
+        colormap bone;
+        drawnow;
+        title('Innitially flipped bits: 0')
+    end
+
+
+    function store(src, event)
+        val = cDist.Value;
+        distortionLevel = round(val);
+        distorted = distortmem(memories(m, :), distortionLevel);
+        img = reshape(distorted, 8, 8);
+        imagesc(img); 
+        axis square;
+        colormap bone;
+        drawnow;
+        title(['Initially flipped bits: ', num2str(distortionLevel)]);
+        
+    end
+
+    function reconstruct(src,event)
+        net = HopfieldNet(64,rule);
+        net = net.train(memories);
+        net.reconstructVisually(distorted);
+    end
+end
